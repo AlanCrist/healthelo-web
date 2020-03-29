c83fa830b2054c6feb64e1e30bd9430134dce08f
Alan <lanzn16@outlook.com>
2020-03-29 11:54:05 -0300

    add receipt of incident list

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import PowerSettingsNewIcon from "@material-ui/icons/PowerSettingsNew";
 import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+
+import api from "../../services/api";
 
 import "./styles.css";
 
 import logoImg from "../../assets/logo.svg";
 
 export default function Profile() {
+  const [incidents, setIncidents] = useState([]);
+
+  const hospitalId = localStorage.getItem("hospitalId");
+  const hospitalName = localStorage.getItem("hospitalName");
+
+  useEffect(() => {
+    api
+      .get("profile", {
+        headers: {
+          Authorization: hospitalId
+        }
+      })
+      .then(resp => {
+        setIncidents(resp.data);
+      });
+  }, [hospitalId]);
+
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: hospitalId
+        }
+      });
+
+      setIncidents(incidents.filter(incident => incident.id !== id));
+    } catch (err) {
+      alert("Erro ao tentar deletar caso, tente novamente.");
+    }
+  }
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoImg} alt="healthelo" />
-        <span>Bem vinda, Nossa senhora das neves</span>
+        <span>{`Bem vinda, ${hospitalName}`}</span>
 
         <Link className="button" to="/incidents/new">
           Cadastrar novo caso
@@ -24,63 +57,32 @@ export default function Profile() {
       </header>
 
       <h1>Casos cadastrados</h1>
+
       <ul>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
+        {incidents.map(incident => (
+          <li key={incident.id}>
+            <strong>CASO:</strong>
+            <p>{incident.title}</p>
 
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
+            <strong>DESCRIÇÃO:</strong>
+            <p>{incident.description}</p>
 
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
+            <strong>VALOR:</strong>
+            <p>
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+              }).format(incident.value)}
+            </p>
 
-          <button type="button">
-            <DeleteOutlineIcon style={{ color: "#1c2f5d", fontSize: 25 }} />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <DeleteOutlineIcon style={{ color: "#1c2f5d", fontSize: 25 }} />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <DeleteOutlineIcon style={{ color: "#1c2f5d", fontSize: 25 }} />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <DeleteOutlineIcon style={{ color: "#1c2f5d", fontSize: 25 }} />
-          </button>
-        </li>
+            <button
+              onClick={() => handleDeleteIncident(incident.id)}
+              type="button"
+            >
+              <DeleteOutlineIcon style={{ color: "#1c2f5d", fontSize: 25 }} />
+            </button>
+          </li>
+        ))}
       </ul>
     </div>
   );
